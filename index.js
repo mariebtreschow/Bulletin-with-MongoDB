744a203e9a9a42767869c9e9d0a4da9c981d3c5c
Marie Treschow <treschow.marie@gmail.com>
2017-02-08 20:15:13 +0100

    setting up project with handelbars instead

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-      pug = require('pug'),
+      pug = require('hbs'),
       bodyParser = require('body-parser'),
       displayRoutes = require('express-routemap')
       morgan = require('morgan'),
@@ -17,25 +17,24 @@ MongoClient.connect("mongodb://localhost:27017/mongo-app", function(err, db) {
 });
 
 var app = express();
-require('./seed');
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.set('view engine', 'pug');
+app.set('view engine', 'hbs');
 
-app.get('/messages', (req, res) => {
- res.render('messages');
+app.get('/', (req, res) => {
+ res.render('show');
 });
-
 
 app.get('/new', (req, res) => {
    res.render('new');
 });
 
 app.post('/new', (req, res) => {
+   //post message to mongoDB
 });
 
 
